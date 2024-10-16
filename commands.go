59475fd619dddca23a40899cdc428e6fb266e1b3
dim0x69 <github@dsmail.org>
2024-10-16 09:43:01 +0200

    Add templating to commands.

--- commands.go
+++ commands.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 	"os/exec"
+	"regexp"
+	"text/template"
 
 	"github.com/sirupsen/logrus"
 	"github.com/yuin/goldmark"
@@ -11,83 +14,122 @@ import (
 	"github.com/yuin/goldmark/text"
 )
 
-// executeCommand writes the command code to a temporary file and executes it
-func executeCommand(commandName string) error {
+func executeCommand(commandName string, args ...string) error {
+	logrus.Debug(fmt.Sprintf("Executing command %s with args %v", commandName, args))
 
-    commandBlock, ok := commands[commandName]
-    if !ok {
-        return fmt.Errorf("command not found: %s", commandName)
-    }
+	commandBlock, ok := commands[commandName]
+	if !ok {
+		return fmt.Errorf("command not found: %s", commandName)
+	}
 
-    launcher, ok := launchers[commandBlock.Lang]
-    if !ok {
-        return fmt.Errorf("launcher not found for language: %s", commandBlock.Lang)
-    }
+	launcher, ok := launchers[commandBlock.Lang]
+	if !ok {
+		return fmt.Errorf("launcher not found for language: %s", commandBlock.Lang)
+	}
 
-    // Write the command.code to the temporary file
-    tmpFile, err := os.CreateTemp("", fmt.Sprintf("mdx-*.%s", launcher.extension))
-    if err != nil {
-        return fmt.Errorf("failed to create temporary file: %v", err)
-    }
-    defer os.Remove(tmpFile.Name())
+	// Create a map for the template arguments
+	argMap := make(map[string]string)
+	for i, arg := range args {
+		argMap[fmt.Sprintf("arg%d", i+1)] = arg
+	}
 
-    if _, err := tmpFile.Write([]byte(commandBlock.Code)); err != nil {
-        return fmt.Errorf("failed to write to temporary file: %v", err)
-    }
-    if err := tmpFile.Close(); err != nil {
-        return fmt.Errorf("failed to close temporary file: %v", err)
-    }
+	// Validate that all placeholders in the template are provided in args and vice versa
+	placeholderPattern := regexp.MustCompile(`{{\s*\.arg(\d+)\s*}}`)
+	matches := placeholderPattern.FindAllStringSubmatch(commandBlock.Code, -1)
 
-    cmd := exec.Command(launcher.cmd, tmpFile.Name())
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
+	placeholderSet := make(map[string]struct{})
+	for _, match := range matches {
+		placeholderSet[match[1]] = struct{}{}
+	}
 
-    if err := cmd.Run(); err != nil {
-        return fmt.Errorf("failed to execute command: %v", err)
-    }
+	for i := range args {
+		argKey := fmt.Sprintf("%d", i+1)
+		if _, ok := placeholderSet[argKey]; !ok {
+			return fmt.Errorf("argument %d (\"%s\") is provided but not used in the template", i+1, args[i])
+		}
+	}
 
-    return nil
+	for placeholder := range placeholderSet {
+		argIndex := fmt.Sprintf("arg%s", placeholder)
+		if _, ok := argMap[argIndex]; !ok {
+			return fmt.Errorf("{{.arg%s}} is used in command \"%s\" but not provided in args", placeholder, commandName)
+		}
+	}
+
+	// Parse and execute the template
+	tmpl, err := template.New("command").Parse(commandBlock.Code)
+	if err != nil {
+		return fmt.Errorf("failed to parse template: %v", err)
+	}
+
+	var renderedCode bytes.Buffer
+	err = tmpl.Execute(&renderedCode, argMap)
+	if err != nil {
+		return fmt.Errorf("failed to execute template: %v", err)
+	}
+
+	// Write the rendered code to the temporary file
+	tmpFile, err := os.CreateTemp("", fmt.Sprintf("mdx-*.%s", launcher.extension))
+	if err != nil {
+		return fmt.Errorf("failed to create temporary file: %v", err)
+	}
+	defer os.Remove(tmpFile.Name())
+
+	if _, err := tmpFile.Write(renderedCode.Bytes()); err != nil {
+		return fmt.Errorf("failed to write to temporary file: %v", err)
+	}
+	if err := tmpFile.Close(); err != nil {
+		return fmt.Errorf("failed to close temporary file: %v", err)
+	}
+
+	cmd := exec.Command(launcher.cmd, tmpFile.Name())
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("failed to execute command: %v", err)
+	}
+
+	return nil
 }
 
 func loadCommands(markdownFile string) error {
-    source, err := os.ReadFile(markdownFile)
-    if err != nil {
-        return err
-    }
+	source, err := os.ReadFile(markdownFile)
+	if err != nil {
+		return err
+	}
 
-    md := goldmark.New()
-    reader := text.NewReader(source)
-    doc := md.Parser().Parse(reader)
+	md := goldmark.New()
+	reader := text.NewReader(source)
+	doc := md.Parser().Parse(reader)
 
-    var currentHeading string
-    var foundCodeBlock bool
+	var currentHeading string
+	var foundCodeBlock bool
 
-    ast.Walk(doc, func(n ast.Node, entering bool) (ast.WalkStatus, error) {
-        if heading, ok := n.(*ast.Heading); ok && entering {
-            if heading.Level <= 2 {
-                currentHeading = string(heading.Text(source))
-                logrus.Debug("Found heading:", currentHeading)
-                foundCodeBlock = false
-            }
-        }
+	ast.Walk(doc, func(n ast.Node, entering bool) (ast.WalkStatus, error) {
+		if heading, ok := n.(*ast.Heading); ok && entering {
+			if heading.Level <= 2 {
+				currentHeading = string(heading.Text(source))
+				logrus.Debug("Found heading:", currentHeading)
+				foundCodeBlock = false
+			}
+		}
 
-        if block, ok := n.(*ast.FencedCodeBlock); ok && entering && !foundCodeBlock {
-            if currentHeading != "" {
-                lang := string(block.Language(source))
-                code := string(block.Text(source))
+		if block, ok := n.(*ast.FencedCodeBlock); ok && entering && !foundCodeBlock {
+			if currentHeading != "" {
+				lang := string(block.Language(source))
+				code := string(block.Text(source))
 
-                commands[currentHeading] = CommandBlock{
-                    Lang:   lang,
-                    Code:   code,
-                    Config: make(map[string]string),
-                }
-                foundCodeBlock = true
-                logrus.Debug(fmt.Sprintf("Found code block. Infostring: %s, Heading: %s",lang, currentHeading))
-            }
-        }
-        return ast.WalkContinue, nil
-    })
-    return nil
+				commands[currentHeading] = CommandBlock{
+					Lang:   lang,
+					Code:   code,
+					Config: make(map[string]string),
+				}
+				foundCodeBlock = true
+				logrus.Debug(fmt.Sprintf("Found code block. Infostring: %s, Heading: %s", lang, currentHeading))
+			}
+		}
+		return ast.WalkContinue, nil
+	})
+	return nil
 }
-
-
--- main.go
+++ main.go
@@ -11,63 +11,63 @@ import (
 
 // CommandBlock represents a parsed command block
 type CommandBlock struct {
-    Lang   string // the infostring from the code fence
-    Code   string // the content of the code fence
-	Args  []string // placeholder for the future
-    Config map[string]string // placeholder for the future
+	Lang   string            // the infostring from the code fence
+	Code   string            // the content of the code fence
+	Args   []string          // placeholder for the future
+	Config map[string]string // placeholder for the future
 }
 
 // Global store for parsed commands
 var commands = map[string]CommandBlock{}
 
-
 type LauncherBlock struct {
-	cmd string // The command to execute for the infostring above
+	cmd       string // The command to execute for the infostring above
 	extension string // The file extension for the language
 }
+
 var launchers = map[string]LauncherBlock{}
 
 func isExecutableInPath(candidates []string) string {
-    for _, cmd := range candidates {
-        if _, err := exec.LookPath(cmd); err == nil {
-            return cmd
-        }
-    }
-    return ""
+	for _, cmd := range candidates {
+		if _, err := exec.LookPath(cmd); err == nil {
+			return cmd
+		}
+	}
+	return ""
 }
 
 func loadLaunchers() {
-    addedLaunchers := []string{}
+	addedLaunchers := []string{}
 
-    if cmd := isExecutableInPath([]string{"sh"}); cmd != "" {
-        launchers["sh"] = LauncherBlock{cmd: cmd, extension: "sh"}
-        addedLaunchers = append(addedLaunchers, cmd)
-    }
+	if cmd := isExecutableInPath([]string{"sh"}); cmd != "" {
+		launchers["sh"] = LauncherBlock{cmd: cmd, extension: "sh"}
+		addedLaunchers = append(addedLaunchers, cmd)
+	}
 
-    pythonCandidates := []string{"python", "python3"}
-    if cmd := isExecutableInPath(pythonCandidates); cmd != "" {
-        launchers["python"] = LauncherBlock{cmd: cmd, extension: "py"}
-        addedLaunchers = append(addedLaunchers, cmd)
-    }
-    
+	pythonCandidates := []string{"python", "python3"}
+	if cmd := isExecutableInPath(pythonCandidates); cmd != "" {
+		launchers["python"] = LauncherBlock{cmd: cmd, extension: "py"}
+		addedLaunchers = append(addedLaunchers, cmd)
+	}
+
 	// Add more binaries as needed
 
-    // Print added launchers
-    logrus.Debug("Added launchers:", addedLaunchers)
+	// Print added launchers
+	logrus.Debug("Added launchers:", addedLaunchers)
 }
 
 func setLogLevel() {
-    logLevel := os.Getenv("MDX_LOG_LEVEL")
-    switch logLevel {
-    case "DEBUG":
-        logrus.SetLevel(logrus.DebugLevel)
-    case "INFO":
-        logrus.SetLevel(logrus.InfoLevel)
-    case "ERROR":
-        logrus.SetLevel(logrus.ErrorLevel)
-    default:
-        logrus.SetLevel(logrus.WarnLevel)
-    }
+	logLevel := os.Getenv("MDX_LOG_LEVEL")
+	switch logLevel {
+	case "DEBUG":
+		logrus.SetLevel(logrus.DebugLevel)
+	case "INFO":
+		logrus.SetLevel(logrus.InfoLevel)
+	case "ERROR":
+		logrus.SetLevel(logrus.ErrorLevel)
+	default:
+		logrus.SetLevel(logrus.WarnLevel)
+	}
 }
 
 func errorExit(format string, args ...interface{}) {
@@ -77,44 +77,48 @@ func errorExit(format string, args ...interface{}) {
 
 func main() {
 	setLogLevel()
-    // Define command-line flags
-    // listAllCommandsFlag := flag.Bool("all", false, "List all commands, even if extension is not installed")
+	// Define command-line flags
+	// listAllCommandsFlag := flag.Bool("all", false, "List all commands, even if extension is not installed")
 	flag.Parse()
 	logrus.Debug("MDX started with parameters:", os.Args)
-    // Check for subcommands
-    if flag.NArg() > 0 {
-        subcommand := flag.Arg(0)
-        switch subcommand {
-        // case "list":
-        //     listCommands(*listAllCommandsFlag)
-        //     return
+	// Check for subcommands
+	if flag.NArg() > 0 {
+		subcommand := flag.Arg(0)
+		switch subcommand {
+		// case "list":
+		//     listCommands(*listAllCommandsFlag)
+		//     return
 		default:
 			if flag.NArg() < 2 {
 				errorExit("Usage: mdx <markdown-file> [command] [args]")
 			}
 			// Assume the first argument is a markdown file
-            markdownFile := subcommand
+			markdownFile := subcommand
 			command_name := flag.Arg(1)
+			command_args := []string{}
+			if flag.NArg() > 2 {
+				command_args = flag.Args()[2:]
+			}
 
 			loadLaunchers()
-            // Load the commands from the markdown file into the global structure
-            err := loadCommands(markdownFile)
+			// Load the commands from the markdown file into the global structure
+			err := loadCommands(markdownFile)
 			if err != nil {
-                errorExit("Error loading commands from %s: %v", markdownFile, err)
-            }
+				errorExit("Error loading commands from %s: %v", markdownFile, err)
+			}
 
 			// Test whether command is in commands
-			if _, ok := commands[command_name];ok {
+			if _, ok := commands[command_name]; ok {
 				// Execute the command
-				err := executeCommand(command_name)
+				err := executeCommand(command_name, command_args...)
 				if err != nil {
 					errorExit("Error executing command: %v", err)
 				}
-			}else {
-				errorExit("Command not found in %s: %s", markdownFile, command_name, )
+			} else {
+				errorExit("Command not found in %s: %s", markdownFile, command_name)
 			}
 			return
-        }
-    }
+		}
+	}
 	fmt.Print("Usage: mdx <markdown-file> [command] [args] or mdx list [-all]")
 }
