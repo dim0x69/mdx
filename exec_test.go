2dce9609bb98d410d4ea26337d4a82ec67a6f577
Dimitrios Slamaris <github@dsmail.org>
2024-10-21 07:14:43 +0200

    Add exec_test.go

---
+++ exec_test.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"io"
+	"os"
+	"testing"
+)
+
+func captureOutput(f func() error) (string, error) {
+	var buf bytes.Buffer
+	stdout := os.Stdout
+	stderr := os.Stderr
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+	os.Stderr = w
+
+	err := f()
+
+	w.Close()
+	io.Copy(&buf, r)
+	os.Stdout = stdout
+	os.Stderr = stderr
+
+	return buf.String(), err
+}
+func TestExecuteCodeBlock_ValidCodeBlockExecution(t *testing.T) {
+	launchers = map[string]LauncherBlock{"sh": {"sh", "sh"}, "bash": {"sh", "sh"}}
+	codeBlock := CodeBlock{
+		Lang: "sh",
+		Code: `echo "Hello, {{.arg1}}"`,
+		Meta: map[string]interface{}{"shebang": false},
+	}
+	args := []string{"World"}
+	var wantErr error = nil
+
+	output, err := captureOutput(func() error {
+		return executeCodeBlock(codeBlock, args...)
+	})
+
+	expectedOutput := "Hello, World\n"
+	if output != expectedOutput {
+		t.Errorf("executeCodeBlock() output = %v, expectedOutput %v", output, expectedOutput)
+	}
+
+	if wantErr != nil {
+		if !errors.Is(err, wantErr) {
+			t.Errorf("executeCodeBlock() error = %v, wantErr %v", err, wantErr)
+		}
+	} else if err != nil {
+		t.Errorf("executeCodeBlock() error = %v, wantErr %v", err, wantErr)
+	}
+}
+
+func TestExecuteCodeBlock_MissingArgument(t *testing.T) {
+	codeBlock := CodeBlock{
+		Lang: "sh",
+		Code: `echo "Hello, {{.arg1}}"`,
+		Meta: map[string]interface{}{"shebang": false},
+	}
+	args := []string{}
+	wantErr := ErrArgUsedInTemplateNotProvided
+
+	err := executeCodeBlock(codeBlock, args...)
+
+	if wantErr != nil {
+		if !errors.Is(err, wantErr) {
+			t.Errorf("executeCodeBlock() error = %v, wantErr %v", err, wantErr)
+		}
+	} else if err != nil {
+		t.Errorf("executeCodeBlock() error = %v, wantErr %v", err, wantErr)
+	}
+}
+
+func TestExecuteCodeBlock_UnusedArgument(t *testing.T) {
+	codeBlock := CodeBlock{
+		Lang: "sh",
+		Code: `echo "Hello, {{.arg1}}"`,
+		Meta: map[string]interface{}{"shebang": false},
+	}
+	args := []string{"World", "Extra"}
+	wantErr := ErrArgProvidedButNotUsed
+
+	err := executeCodeBlock(codeBlock, args...)
+
+	if wantErr != nil {
+		if !errors.Is(err, wantErr) {
+			t.Errorf("executeCodeBlock() error = %v, wantErr %v", err, wantErr)
+		}
+	} else if err != nil {
+		t.Errorf("executeCodeBlock() error = %v, wantErr %v", err, wantErr)
+	}
+}
+
+func TestExecuteCodeBlock_TemplateParsingError(t *testing.T) {
+	codeBlock := CodeBlock{
+		Lang: "sh",
+		Code: `echo "Hello, {{.arg1"`, // Missing closing braces
+		Meta: map[string]interface{}{"shebang": false},
+	}
+	args := []string{"World"}
+	wantErr := ErrArgProvidedButNotUsed
+
+	err := executeCodeBlock(codeBlock, args...)
+
+	if wantErr != nil {
+		if !errors.Is(err, wantErr) {
+			t.Errorf("executeCodeBlock() error = %v, wantErr %v", err, wantErr)
+		}
+	} else if err != nil {
+		t.Errorf("executeCodeBlock() error = %v, wantErr %v", err, wantErr)
+	}
+}
+
+func TestExecuteCodeBlock_LauncherNotDefined(t *testing.T) {
+	codeBlock := CodeBlock{
+		Lang: "unknown",
+		Code: `echo "Hello, {{.arg1}}"`,
+		Meta: map[string]interface{}{"shebang": false},
+	}
+	args := []string{"World"}
+	wantErr := ErrNoLauncherDefined
+
+	err := executeCodeBlock(codeBlock, args...)
+
+	if wantErr != nil {
+		if !errors.Is(err, wantErr) {
+			t.Errorf("executeCodeBlock() error = %v, wantErr %v", err, wantErr)
+		}
+	} else if err != nil {
+		t.Errorf("executeCodeBlock() error = %v, wantErr %v", err, wantErr)
+	}
+}
